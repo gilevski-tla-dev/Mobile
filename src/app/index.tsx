ec8c95a3d7a5ae03ffa368f1a760c397d1831382
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2025-02-09 23:40:25 +0200

    upd

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,17 +3,21 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "./providers/ThemeProvider";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const queryClient = new QueryClient();
 
 export default function AppContainer() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <NavigationContainer>
-          <Tabs />
-        </NavigationContainer>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <NavigationContainer>
+            <Tabs />
+          </NavigationContainer>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </Provider>
   );
 }
